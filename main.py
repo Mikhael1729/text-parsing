a5c85b825792daff0f1dbea8bbffff830b0fddde
Denys Rosario <drosario@truora.com>
2020-12-05 23:57:13 -0400

    avoid infinite loop

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 from representation import Graph
-
-words = [
-  "para",
-  "de",
-  "uno",
-  "la",
-  "es",
-  "luna",
-  "sol",
-  "carro",
-  "hola",
-  "arbol",
-  "silla",
-  "mesa",
-  "casa",
-  "año",
-  "rio",
-  "mesa",
-  "perro",
-  "calle",
-  "feliz",
-  "son"
-]
+from representation import heuristic
+from representation import KEY_WORDS
+# words = [
+#   "para",
+#   "de",
+#   "uno",
+#   "la",
+#   "es",
+#   "luna",
+#   "sol",
+#   "carro",
+#   "hola",
+#   "arbol",
+#   "silla",
+#   "mesa",
+#   "casa",
+#   "año",
+#   "rio",
+#   "mesa",
+#   "perro",
+#   "calle",
+#   "feliz",
+#   "son"
+# ]
 
 # S = Graph()
 # s1 = S.add_node("p")
@@ -47,14 +48,64 @@ def connect2(word, graph):
     letter = word[i]
     graph.add_edge(nodes[i - 1].id, nodes[i].id, letter)
 
+
 def generate_states_space2():
   S = Graph()
 
-  for word in words:
+  for word in KEY_WORDS:
     connect2(word, S)
 
   return S
 
 generated = generate_states_space2()
 for key, value in generated.nodes.items():
-  print(value)
+#   print(value)
+    continue
+
+def bfs(graph, goal_word):
+    open_l = []
+    closed_l = []
+    not_modified_open = []
+    not_modified_closed = []
+
+    open_l.append(graph.nodes[0])
+    not_modified_open.append(graph.nodes[0])
+    times = 0
+
+    while True:
+        if len(open_l) == 0:
+            return False
+        
+        min_h = 10
+        min_n = open_l[0]
+        # gets the node with the lowest value of h(n)
+        for node in open_l:
+            current_h = heuristic(node)
+            if current_h < min_h:
+                min_h = current_h
+                min_n = node
+                # print(min_n)
+
+        open_l.remove(min_n)
+        closed_l.append(min_n)
+        not_modified_closed.append(min_n)
+
+        # check if any successor is goal node or not
+        childs = min_n.adjacents
+        for child in childs:
+            if child.destination.value == goal_word:
+                return True
+
+        for child in childs:
+            if child not in not_modified_open and child not in not_modified_closed:
+                open_l.append(child.destination)
+                not_modified_open.append(child.destination)
+
+        times += 1
+        if times > len(graph.edges):
+            return False
+
+text = "Uno De Hola Denys Mikhael Erica Luna"
+
+for word in text.split(" "):
+    print(word + ': ' + str(bfs(generated, word)))
